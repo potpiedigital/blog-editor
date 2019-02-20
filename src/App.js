6b2fe94efb80caf04da2e7e2ab43aef7022e86e1
Mark Hayes <mark.m.hayes1@gmail.com>
2019-02-19 20:54:27 -0500

    adding in an initial pass of blocks in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  state = { blocks: ['hello world', 'another string'] };
   render() {
-    return <div> </div>;
+    return (
+      <div>
+        {this.state.blocks.map(block => {
+          return <p key={block}>{block}</p>;
+        })}
+      </div>
+    );
   }
 }
 
