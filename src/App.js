e673f1e6401059fed68317ad9b1294248bf6c759
Mark Hayes <mark.m.hayes1@gmail.com>
2019-02-19 21:05:44 -0500

    adding homework in comments

--- src/App.js
+++ src/App.js
@@ -15,3 +15,6 @@ class App extends Component {
 }
 
 export default App;
+
+// figure out how to render a blocks object using either a p tag or an h1 tag.
+// hint: switch statement that returns a different component based on value inside block object
